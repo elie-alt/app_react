455a437768efe08c8dfce6c8351b9201c440da41
elie-alt <elie.archambault3@gmail.com>
2023-12-15 13:27:45 +0100

    new

---
+++ src/Products.js
@@ -0,0 +1,13 @@
+// const PRODUCTS = [
+//     { category: "Fruits", price: "1 €", stocked: true, name: "Pomme" },
+//     { category: "Fruits", price: "1 €", stocked: true, name: "Fruit du dragon" },
+//     { category: "Fruits", price: "2 €", stocked: false, name: "Fruit de la passion" },
+//     { category: "Légumes", price: "2 €", stocked: true, name: "Épinard" },
+//     { category: "Légumes", price: "4 €", stocked: false, name: "Citrouille" },
+//     { category: "Légumes", price: "1 €", stocked: true, name: "Petits pois" }
+// ];
+
+// export default function App() {
+//     return <FilterableProductTable products=  />;
+// }
+   
